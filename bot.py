711c29d5fb545f993eecfeb7b1ddc36e2c39830a
Sasidhar Turaga <45069227+Sasidhart97@users.noreply.github.com>
2020-12-02 06:40:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,16 @@ from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
 import torch
 from model1 import models
-from sentence_transformers import SentenceTransformer, util
-saved_model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
+#from sentence_transformers import SentenceTransformer, util
+#saved_model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
 pid=["Hello and Welcome"]
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-    global saved_model
+    #global saved_model
     global pid
     async def on_message_activity(self, turn_context: TurnContext):
         pid.append(str(turn_context.activity.text))
-        await turn_context.send_activity(models(turn_context.activity.text,saved_model,pid[-2]))
+        await turn_context.send_activity(models(turn_context.activity.text,pid[-2]))
         
     async def on_members_added_activity(
         self,
