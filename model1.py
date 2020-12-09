28c070cb23d74cc2292a631e788ad630dd1b127c
Sasidhar Turaga <45069227+Sasidhart97@users.noreply.github.com>
2020-12-09 11:52:04 +0530

    Update model1.py

--- model1.py
+++ model1.py
@@ -1,63 +1,15 @@
-import scipy
-import numpy as np
-import win32com.client as win32
-import pandas as pd
-import re
-import nltk
-nltk.download('wordnet')
-nltk.download('punkt')
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
-from nltk.corpus import stopwords
-import socket
+from gensim.parsing.preprocessing import remove_stopwords, preprocess_string
+from gensim.utils import simple_preprocess
+from gensim.models import FastText
+def embed():
+    #que=pd.read_excel("data/questionnare.xlsx")
+    corpus=['How To Add Photo and other Details', 'How To Add Proxy In Internet Explorer', 'How to add Signature in Outlook', 'How to raise JIRA ticket', 'How To map Home drive', 'How to map network drive', 'how to reset windows password', 'How To Use RSA Token', 'how to reset outlook password', 'vpn connection failed error', 'my system is slow', 'how to map share drive', 'windows account locked', 'access to share drive', 'Hello', 'Hi', 'Bye', 'Who created you']
+    solution=['For adding photo and details open the file :  "DataTeams -> Chatbot Channel -> files -> How To Add Photo and other Details.pptx"', 'For adding proxy in internet explorer open the file :  "DataTeams -> Chatbot Channel -> files -> How To Add Proxy In Internet Explorer.pptx"', 'For Adding signature in outlook open the file :  "DataTeams -> Chatbot Channel -> files -> How to add Signature in Outlook.pptx"', 'To raise jira ticket open the file :  "DataTeams -> Chatbot Channel -> files -> How to raise JIRA ticket.pptx"', 'To map home drive open the file :  "DataTeams -> Chatbot Channel -> files -> How To map Home drive.pptx"', 'To map network drive open the file :  "DataTeams -> Chatbot Channel -> files -> How to map network drive.pptx"', 'To reset windows password open the file :  "DataTeams -> Chatbot Channel -> files -> how to reset windows password.pptx"', 'To use rsa token open the file :  "DataTeams -> Chatbot Channel -> files -> How To Use RSA Token.pptx"', 'To reset outlook password open the file :  "DataTeams -> Chatbot Channel -> files -> how to reset outlook password.pptx"', 'Please check whether your internet connection is stable or not and try restarting your system. If issue still prevails please contact IT team', 'Please restart your system and check your internet connectivity.  For application slowness need to contact with application team. If it is related to outlook, excel and overall VDI slowness contact to IT team.', 'To map the drive> open this PC>click on computer>Click on MAP Network drive>choose later>Paste the server path', 'Wait for 5 min account get unlocked autometicatly. If still not unlocked connect to IT team and they will unlock the account', 'connect to IT and they will provide you the access for share drive', 'Hello', 'Hi', 'Bye', 'ML Team from CI, having said that I have a special interest in sasidhar :)']
+    corps1=[]
+    for i in corpus:
+        corps1.append(simple_preprocess(remove_stopwords(i.lower())))
 
-def models(text1,message):
-    if(text1.lower().replace(" ","")=="connecttoit"):
-        server = smtplib.SMTP('64.233.184.108')
-        server.ehlo()
-        server.starttls()
-        server.login('sasidharturaga@gmail.com', 'felixsasi1997')
-        body = "Please resolve it ----- " + message
-        msg = MIMEText(body,'plain','utf-8')
-        subject = 'Email test'
-        msg["Subject"] = Header(subject, 'utf-8')
-        from1 = 'sasidharturaga@gmail.com'
-        to = 'turaga.sasidhar@metro-services.in'
-        msg["From"] = Header(from1, 'utf-8')
-        msg["To"] = Header(to, 'utf-8')
-        txt = msg.as_string()
-        server.sendmail(from1, to, txt)
-        #import pythoncom
-        #pythoncom.CoInitialize()
-        #outlook = win32.Dispatch('outlook.application')
-        #mail = outlook.CreateItem(0)
-        #mail.To = 'turaga.sasidhar@metro-services.in'
-        #mail.Subject = 'Unresolved Query from IT Bot'
-        #mail.Body = '***This is an auto-generated mail from IT BOT.***\n\nThis is an automated email from the Chatbot since it couldn\'t address the query from the user.\nPlease find below the query that the user was looking to resolve through the chatbot: "'+message+'"'
-        #mail.Send()
-        return "Mail sent. Someone from IT team will contact you"    
-    else:
-        
-        #from sentence_transformers import SentenceTransformer
-        #import warnings
-        
-        #warnings.filterwarnings("ignore")
-        #model = SentenceTransformer('distilbert-base-nli-stsb-mean-tokens')
-        def clean_text(text):
-            que=pd.read_excel("questionnare.xlsx")
-            corpus=list(que["Question"])
-            solution=list(que["Answer"])
-            stopwords_english = stopwords.words('english')
-            stopwords_english=list(stopwords_english)
-            #print(stopwords_english)
-            find_words = re.compile(r'\w+').findall
-            text=text.lower()
-            temp1=find_words(text)
-            ct=[]
-            for word in temp1:
-                if (word not in stopwords_english):
-                    #print(word)
-                    ct.append(word.lower())
-            return ' '.join(ct)+" "+solution[-1]
-        return clean_text(text1)
+    model = FastText(size=50,window=3, min_count=1)  # instantiate
+    model.build_vocab(sentences=corps1)
+    model.train(sentences=corps1, total_examples=len(corps1), epochs=10)
+    return model,corps1,solution
